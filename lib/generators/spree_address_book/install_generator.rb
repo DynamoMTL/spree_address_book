de42a954c243547758faa89b64002694f6ed5c23
Calvin Yu <calvin@scoutmob.com>
2014-12-12 18:28:50 -0500

    Updated generator to use updated asset paths
    - application.js/css were moved to vendor/assets updated generator
      to look for them there.  The assets should probably be moved to
      the new spree/frontend paths, but wanted to minimize any breaking
      changes
    - Also fixed a small typo ('skiping')

--- lib/generators/spree_address_book/install_generator.rb
+++ lib/generators/spree_address_book/install_generator.rb
@@ -2,13 +2,13 @@ module SpreeAddressBook
   module Generators
     class InstallGenerator < Rails::Generators::Base
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_address_book\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require store/spree_address_book\n"
       end
-      
+
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/spree_address_book\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", " *= require store/spree_address_book\n", :before => /\*\//, :verbose => true
       end
-    
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_address_book'
       end
@@ -18,7 +18,7 @@ module SpreeAddressBook
          if res == "" || res.downcase == "y"
            run 'bundle exec rake db:migrate'
          else
-           puts "Skiping rake db:migrate, don't forget to run it!"
+           puts "Skipping rake db:migrate, don't forget to run it!"
          end
       end
     end
